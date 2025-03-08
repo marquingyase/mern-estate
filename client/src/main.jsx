bdc10356b779c20a2601dea7e8b30eff4bedf9c4
marquingyase <sethgyan587@gmail.com>
2025-03-08 22:54:46 +0000

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import { persistReducer , persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  user: userReducer, // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other reducers here, like: homeReducer, cartReducer, etc.  // Add your other
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  }, // Your reducers go here
+  reducer: persistedReducer, // Your reducers go here
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // Enable this to allow actions with non-serializable payloads
@@ -13,3 +25,5 @@ export const store = configureStore({
       // other middleware you want to use...
     }), // Add any middleware you need here
 });
+
+export const persistor = persistStore(store);
